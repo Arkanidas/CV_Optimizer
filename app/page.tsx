2f4746aad75b730b702de5b9b0c8eb1d4b9c6288
Leon <xleonidasprox@gmail.com>
2026-05-01 04:27:26 +0700

    testing

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-row flex-3 items-center justify-center bg-red-400 font-sans dark:bg-red">
+    <h1>CV Optimizer</h1>
+    </div>
+  );
+}
